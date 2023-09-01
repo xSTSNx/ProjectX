c6248de941bba6d0f6dd05375a5c69858be0f9da
xSTSNx <stefan_sonnenschein@yahoo.de>
2023-09-01 16:36:11 +0200

    index change

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use((req, res, next) => {                           //define and pass a middleware function//
-    console.log('Time: ', Date.now());
-    next();                                             //next() tell the middleware to go to the next middleware function if there is one (important to include at the end!!)//
-  });
+app.use('/openai', require ('./routes/openaiRoutes'))
 
 app.listen(port, () => console.log(`Example app is listening on port ${port}`));  //Tells the app to listen to Port XY//
