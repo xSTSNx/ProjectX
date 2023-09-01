855672c7245a25d316fd12491c23d1545ef73106
xSTSNx <stefan_sonnenschein@yahoo.de>
2023-09-01 16:27:35 +0200

    updates

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Ich verändere den Consolelog")
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use((req, res, next) => {                           //define and pass a middleware function//
+    console.log('Time: ', Date.now());
+    next();                                             //next() tell the middleware to go to the next middleware function if there is one (important to include at the end!!)//
+  });
+
+app.listen(port, () => console.log(`Example app is listening on port ${port}`));  //Tells the app to listen to Port XY//
