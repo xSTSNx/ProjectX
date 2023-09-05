ba481fd4a6b3daef0215b2f081165bfdbe5397ff
xSTSNx <stefan_sonnenschein@yahoo.de>
2023-09-05 11:50:45 +0200

    test

--- controllers/openaicontroller.js
+++ controllers/openaicontroller.js
@@ -1,3 +1,10 @@
+const { OpenAIApi } = require("openai");
+const openai = OpenAIApi({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+
+
 const generateImage = async (req, res) => { 
     res.status(200).json({
         erfolgreich: true,
