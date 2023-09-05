fb5056cccf07adca5be53b8b974546a73b6276ac
Sebky <seb.kyereh@gmail.com>
2023-09-05 14:08:35 +0200

    parcel install + html boilerplate

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 const express = require('express')
 const dotenv = require('dotenv').config();
-const port = process.env.DEV_PORT;
-
+const port = process.env.PORT;
+const path = require('path')
 const app = express();
 
-app.use('/openai', require('./routes/openaiRoutes'))
+app.use('/openai', require ('./routes/openaiRoutes'))
+app.use('/', express.static(__dirname + '/frontend'));
+app.listen(port, () => console.log(`Example app is listining on port ${port}`));  //Tells the app to listen to Port XY//
 
-app.listen(port, () => console.log(`Mein Server läuft  auf Port ${port}`));
 
