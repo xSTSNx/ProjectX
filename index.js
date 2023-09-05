624c2ebc2362d5fac9735a91910d77d1ba7ee5d0
xSTSNx <stefan_sonnenschein@yahoo.de>
2023-09-05 15:35:12 +0200

    #

--- index.js
+++ index.js
@@ -9,4 +9,3 @@ app.use('/openai', require ('./routes/openaiRoutes'))
 app.use('/', express.static(__dirname + '/frontend'));
 app.listen(port, () => console.log(`Example app is listining on port ${port}`));  //Tells the app to listen to Port XY//
 
-
