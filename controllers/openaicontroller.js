4261b0f97e42008be5a7678c5178d4bc736ff918
xSTSNx <stefan_sonnenschein@yahoo.de>
2023-09-07 15:22:23 +0200

    spinner added, imageSize added, html&css updated

--- controllers/openaicontroller.js
+++ controllers/openaicontroller.js
@@ -2,42 +2,40 @@ const OpenAI = require('openai');
 const dotenv = require('dotenv').config();
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 
-const generateImage = async (req, res) => { 
-  /* try {
+const generateImage = async (req, res) => {
+  const {prompt, size} = req.body; 
+  const imageSize = size === "small" ? "256x256" : size === "medium" ? "512x512" : "1024x1024";   //tertiärer operation
+  try {
     const response = await openai.images.generate({
-        prompt: "a white siamese cat with blue eyes and a red hat",
-        n: 1,
-        size: "512x512",
-      });
-      console.log(response)
-      image_url = response.data[0].url;
+      prompt: "a white siamese cat with blue eyes and a red hat",
+      n: 1,
+      size: imageSize,
+    });
+    console.log(response)
+    image_url = response.data[0].url;
 
-        res.status(200).json({
-            erfolgreich: true,
-            data: image_url
-         })
-   } catch (error) {
+    res.status(200).json({
+      erfolgreich: true,
+      data: image_url
+    })
+  } catch (error) {
     if (error.response) {
-        console.log(error.response.status);
-        console.log(error.response.data);
-      } else {
-        console.log(error.message);
-      }
-        res.status(400).json({
-            erfolgreich: false,
-            error: "das Bild konnte nicht generriert werden!"
-            
-        })
-   }*/
-  // console.log(req)
-   res.status(200).json({
-    erfolgreich: true,
- })
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+    res.status(400).json({
+      erfolgreich: false,
+      error: "das Bild konnte nicht generriert werden!"
+
+    })
+  }
   console.log(res)
 }
 
-module.exports = {generateImage};
+module.exports = { generateImage };
