cd5ceb75aaa0bd0b7762647d90558278e5056e2e
brotha.midnight <115979416+sebky@users.noreply.github.com>
2023-09-18 15:13:06 +0200

    Update after_script.sh

--- after_script.sh
+++ after_script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo su
-sudo mv /home/ubuntu/ProjectX/ProtectX/frontend/ /var/www/html/
+sudo mv /home/ubuntu/ProjectX/ProjectX/frontend/ /var/www/html/
 sudo chown www-data:www-data /var/www/html/
-sudo chown www-data:www-data /var/www/html/frontend/index.html
+sudo chown www-data:www-data /var/www/html/index.html
 touch FERTIG
